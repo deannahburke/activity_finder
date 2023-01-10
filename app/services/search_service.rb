dcdbb6f0ec93d56e071aeb6aa253456f69bb64e3
Deannah Burke <deannah@bierstadtlager.com>
2023-01-10 10:50:47 -0700

    Add activity service

---
+++ app/services/search_service.rb
@@ -0,0 +1,7 @@
+class SearchService
+    def self.get_activity(participants)
+        conn = Faraday.new(url: "http://www.boredapi.com/api/activity")
+        response = conn.get("?participants/#{participants}")
+        json = JSON.parse(response.body, symbolize_names: true)
+    end
+end
