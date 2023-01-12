081bf9dacf7d55a6b4e431fe06a140aaf5475031
Deannah Burke <deannah@bierstadtlager.com>
2023-01-12 09:15:43 -0700

    Add sad path for invalid number of participants test

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -8,4 +8,14 @@ RSpec.describe "Welcome Page" do
         expect(page).to have_field(:num_participants)
         expect(page).to have_button("Find Activities")
     end 
+
+    it 'flashes error message when API returns no activity for given number of participants' do 
+        visit root_path
+
+        fill_in :num_participants, with: (-2)
+        click_button "Find Activities"
+
+        expect(current_path).to eq(root_path)
+        expect(page).to have_content("There are no activities for that number of participants")
+    end
 end 
